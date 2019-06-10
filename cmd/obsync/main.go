e5bf03d181cdc06a50f3345d6a36d7f678a668c2
Ming Cheng <mingcheng@users.noreply.github.com>
2019-06-10 18:32:02 +0800

    增加使用环境变量获取敏感信息

--- cmd/obsync/main.go
+++ cmd/obsync/main.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
-	"strings"
 	"syscall"
 
 	"github.com/mingcheng/obsync.go/util"
@@ -32,49 +31,6 @@ var (
 // print version and build time, then exit
 func PrintVersion() {
 	_, _ = fmt.Fprintf(os.Stderr, "Obsync v%v, built at %v\n%v\n\n", version, date, commit)
-}
-
-// get bucket info, usage and number of files
-func BucketInfo() (info string, err error) {
-	obs := &Obs{
-		BucketName: config.Bucket,
-	}
-
-	if info, err := obs.Info(); err != nil {
-		return "", err
-	} else {
-		return fmt.Sprintf("size %d Kb, %d files", info.Size/1024.0, info.ObjectNumber), nil
-	}
-}
-
-// get obs tasks by directory, ignore "." prefix files
-func ObsTasks(root string) (tasks []*Obs, err error) {
-	var obs []*Obs
-
-	if e := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		// skip directories and dot prefix files
-		if !info.IsDir() && strings.HasPrefix(path, root) && !strings.HasPrefix(info.Name(), ".") {
-			key := path[len(root)+1:]
-			if !strings.HasPrefix(key, ".") {
-				tmp := &Obs{
-					SourceFile: path,
-					RemoteKey:  key,
-					BucketName: config.Bucket,
-				}
-
-				obs = append(obs, tmp)
-			}
-		}
-
-		return nil
-	}); e != nil {
-		return obs, e
-	}
-
-	if config.Debug {
-		log.Printf("size of obs tasks is %d\n", len(obs))
-	}
-	return obs, nil
 }
 
 func main() {
